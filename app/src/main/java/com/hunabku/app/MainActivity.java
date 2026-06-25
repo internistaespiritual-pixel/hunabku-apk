668b5dbf58c1725b788d1364560d0774dce2e874
internistaespiritual-pixel <internistaespiritual@gmail.com>
2026-06-25 04:56:18 -0600

    Refactor MainActivity and integrate Firebase Messaging
    Refactor MainActivity to extend AppCompatActivity and add notification permission handling. Integrate Firebase Messaging to retrieve the FCM token.

--- app/src/main/java/com/hunabku/app/MainActivity.java
+++ app/src/main/java/com/hunabku/app/MainActivity.java
@@ -1,24 +1,58 @@
 package com.hunabku.app;
 
-import android.app.Activity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.firebase.messaging.FirebaseMessaging;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        WebView webView = new WebView(this);
-        setContentView(webView);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+            }
+        }
 
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        android.util.Log.d("FCM_TOKEN", "Token: " + task.getResult());
+                    }
+                });
+
+        webView = findViewById(R.id.webview);
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
-
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://inter2.pythonanywhere.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
