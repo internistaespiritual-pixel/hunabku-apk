69d5acfc07cfffbc5d6fe5355360f1b3b1fe1afb
internistaespiritual-pixel <internistaespiritual@gmail.com>
2026-06-25 03:43:37 -0600

    Add MainActivity with WebView implementation

---
+++ app/src/main/java/com/hunabku/app/MainActivity.java
@@ -0,0 +1,24 @@
+package com.hunabku.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://inter2.pythonanywhere.com");
+    }
+}
